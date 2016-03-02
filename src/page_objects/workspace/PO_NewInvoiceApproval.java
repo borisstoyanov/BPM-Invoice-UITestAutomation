0b9e47ac6a0a119f314c02ab1213d38575fd7629
borisstoyanov <bss.stoyanov@gmail.com>
2016-03-02 14:35:59 +0200

    adding changes

--- src/page_objects/workspace/PO_NewInvoiceApproval.java
+++ src/page_objects/workspace/PO_NewInvoiceApproval.java
@@ -5,52 +5,9 @@ import org.openqa.selenium.By;
 import browser.Browser;
 
 public class PO_NewInvoiceApproval {
-
-	public void addFile() {
-		Browser.instance.findElement(By.xpath("//label[text()='Select your invoice file']//..//..//..//input")).sendKeys("C:\\Dreamix\\bpm\\InvoiceManagement\\BPM-Invoice-UITestAutomation\\testFile.pdf");
+	public PO_NewInvoiceApprovalFluent createinvoice(){
+		return new PO_NewInvoiceApprovalFluent();
 	}
 
-	public void addFile(String filePath) {
-		Browser.instance.findElement(By.xpath("//label[text()='Select your invoice file']//..//..//..//input")).sendKeys(filePath);
-	}
-
-	public void addEntity(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void addVendorNumber(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void selectInvoiceType(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void addInvoiceNumber(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void addInvoiceDescription(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void addCurrency(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void addTotal(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void submit() {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
--- src/page_objects/workspace/PO_NewInvoiceApprovalFluent.java
+++ src/page_objects/workspace/PO_NewInvoiceApprovalFluent.java
@@ -1,15 +1,32 @@
 package page_objects.workspace;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
 
 import browser.Browser;
 
 public class PO_NewInvoiceApprovalFluent {
-	public PO_NewInvoiceApprovalFluent addFile() {
-		Browser.instance.findElement(By.xpath("//label[text()='Select your invoice file']//..//..//..//input"))
-				.sendKeys("C:\\Dreamix\\bpm\\InvoiceManagement\\BPM-Invoice-UITestAutomation\\testFile.pdf");
-		return this;
 
+	private void clear(String id) {
+		Browser.instance.findElement(By.id(id)).clear();
+		
+	}
+	
+	private void sendKeys(String id, String keysToSend){
+		Browser.instance.findElement(By.id(id)).sendKeys(keysToSend);
+		Browser.instance.findElement(By.id(id)).sendKeys(Keys.RETURN);
+
+	}
+	
+	private void elemetHasText(String xpath) {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		String text = Browser.instance.findElement(By.xpath(xpath)).getText();
+		Assert.assertFalse(text.equals(null));
 	}
 
 	public PO_NewInvoiceApprovalFluent addFile(String filePath) {
@@ -19,16 +36,20 @@ public class PO_NewInvoiceApprovalFluent {
 
 	}
 
-	public PO_NewInvoiceApprovalFluent addEntity(String string) {
-		// TODO Auto-generated method stub
-
+	public PO_NewInvoiceApprovalFluent addEntity(String keysToSend) {
+		clear("compCodeId::content");
+		sendKeys("compCodeId::content", keysToSend);
+		elemetHasText("//tr[@id='compNameInp']//span");
 		return this;
 
 	}
 
-	public PO_NewInvoiceApprovalFluent addVendorNumber(String string) {
-		// TODO Auto-generated method stub
 
+
+	public PO_NewInvoiceApprovalFluent addVendorNumber(String keysToSend) {
+		clear("vendNumSubmitIT::content");
+		sendKeys("vendNumSubmitIT::content", keysToSend);
+		elemetHasText("//span[@id='vendNmIC']");
 		return this;
 
 	}
