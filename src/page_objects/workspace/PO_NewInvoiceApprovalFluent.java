fd2d6053baee580c25ea08d689acbdab4b652b82
borisstoyanov <bss.stoyanov@gmail.com>
2016-03-02 18:05:04 +0200

    adding changes

--- src/page_objects/workspace/PO_NewInvoiceApprovalFluent.java
+++ src/page_objects/workspace/PO_NewInvoiceApprovalFluent.java
@@ -12,15 +12,15 @@ public class PO_NewInvoiceApprovalFluent {
 
 	private void clear(String xpath) {
 		Browser.instance.findElement(By.xpath(xpath)).clear();
-		
+
 	}
-	
-	private void sendKeys(String xpath, String keysToSend){
+
+	private void sendKeys(String xpath, String keysToSend) {
 		Browser.instance.findElement(By.xpath(xpath)).sendKeys(keysToSend);
 		Browser.instance.findElement(By.xpath(xpath)).sendKeys(Keys.RETURN);
 
 	}
-	
+
 	private void elemetHasText(String xpath) {
 		try {
 			Thread.sleep(1000);
@@ -46,8 +46,6 @@ public class PO_NewInvoiceApprovalFluent {
 
 	}
 
-
-
 	public PO_NewInvoiceApprovalFluent addVendorNumber(String keysToSend) {
 		clear("//input[@id='vendNumSubmitIT::content']");
 		sendKeys("//input[@id='compCodeId::content']", keysToSend);
@@ -57,7 +55,7 @@ public class PO_NewInvoiceApprovalFluent {
 	}
 
 	public PO_NewInvoiceApprovalFluent selectInvoiceType(InvoiceType type) {
-		new Select(Browser.instance.findElement(By.xpath("//select"))).selectByVisibleText(type.toString());		
+		new Select(Browser.instance.findElement(By.xpath("//select"))).selectByVisibleText(type.toString());
 		return this;
 
 	}
@@ -65,35 +63,36 @@ public class PO_NewInvoiceApprovalFluent {
 	public PO_NewInvoiceApprovalFluent addInvoiceNumber(String keysToSend) {
 		clear("//label[text()='Invoice Number']//..//..//input");
 		sendKeys("//label[text()='Invoice Number']//..//..//input", keysToSend);
-		
-		return this;
-
-	}
-
-	public PO_NewInvoiceApprovalFluent addInvoiceDescription(String string) {
-		// TODO Auto-generated method stub
 
 		return this;
 
 	}
 
-	public PO_NewInvoiceApprovalFluent addCurrency(String string) {
-		// TODO Auto-generated method stub
+	public PO_NewInvoiceApprovalFluent addInvoiceDescription(String keysToSend) {
+		clear("//label[text()='Please provide a description of this invoice']//..//input");
+		sendKeys("//label[text()='Please provide a description of this invoice']//..//input", keysToSend);
 
 		return this;
 
 	}
 
-	public PO_NewInvoiceApprovalFluent addTotal(String string) {
-		// TODO Auto-generated method stub
+	public PO_NewInvoiceApprovalFluent addCurrency(String keysToSend) {
+		clear("//input[@id='currIT::content']");
+		sendKeys("//input[@id='currIT::content']", keysToSend);
 
+		return this;
+
+	}
+
+	public PO_NewInvoiceApprovalFluent addTotal(String keysToSend) {
+		clear("//label[text()='Total Amount']//..//..//input");
+		sendKeys("//label[text()='Total Amount']//..//..//input", keysToSend);
 		return this;
 
 	}
 
 	public PO_NewInvoiceApprovalFluent submit() {
-		// TODO Auto-generated method stub
-
+		Browser.instance.findElement(By.xpath("//span[text()='Submit']")).click();
 		return this;
 
 	}
